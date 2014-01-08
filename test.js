fb88eeb59248c10bb2e90fe2cd84ee5358024a52
Michael van der Weg <michael@vanderweg.ch>
2014-01-08 20:16:09 +0100

    query generator basics are working

---
+++ test.js
@@ -0,0 +1,88 @@
+
+	
+	var   Class 			= require('ee-class')
+		, log 				= require('ee-log');
+
+
+
+	var Cluster = require('./')
+
+
+
+
+	var or = function(){
+		var a = new Array(Array.prototype.slice.call(arguments));
+		a.mode = 'or';
+		return a;
+	}
+
+	var and = function(){
+		var a = new Array(Array.prototype.slice.call(arguments));
+		a.mode = 'and';
+		return a;
+	}
+
+
+
+	var cluster = new Cluster({type:'mysql'});
+
+	cluster.addNode('readwrite', {
+		  username: 	'eb-client'
+		, password: 	''
+		, host: 		'10.0.100.1'
+	});
+
+
+	cluster.getConnection(function(err, connection){
+		connection.query( 'query', [
+			{
+				filter: {
+					event: {
+						_: [
+						 	  { id: 5 }
+							, { deleted: null }
+							, [
+								  { id: 66 }
+								, { deleted: function(){ return {fn: 'notNull'} } }
+								, { 
+									  id: 66 
+									, name: 'hui'
+								}
+							]
+						]
+						, created: new Date()
+						, updated: new Date()
+						, tenant:{
+							id:55
+						}
+						, group: [
+							{ id: 3 }
+							, {name: 'fabi fotze'}
+						]
+						, random : function(){
+							return {
+								  operator: 'lte'
+								, query:{
+									filter: {
+										fabian: {
+											  sex: true
+											, withMen: true
+										}
+									}
+									, from: 'vandi'
+									, select: [
+										'id'
+									]
+									, limit: 1
+								}
+							};
+						}
+					}
+				}
+				, from: 'event'
+				, select: ['id', function(){return {fn: 'max', property: 'deleted'}}]
+				, group: ['deleted']
+			}
+		]);
+	});
+
