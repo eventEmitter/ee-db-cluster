6e51533731b13f6da1c17bf262702b1d762a6c9f
Michael van der Weg <michael@vanderweg.ch>
2014-01-08 20:15:59 +0100

    query generator basics are working

--- test/default.js
+++ test/default.js
@@ -1,65 +1,35 @@
 
 	
-	var   Class 		= require('ee-class')
-		, log 			= require('ee-log')
-		, assert 		= require('assert')
-		, travis 		= require('ee-travis');
+	var   Class 			= require('ee-class')
+		, log 				= require('ee-log')
+		, assert 			= require('assert')
+		, travis 			= require('ee-travis');
 
 
 
-	var Schema = require('../')
+	var Cluster = require('../')
 
 
 
 
 
-	describe('The Schema', function(){
-		var schema, user;
+	describe('The Cluster', function(){
+		var cluster;
 
-		it('should emit an «on load» event', function(done){
-			schema = new Schema({
-				  dialect: 	'postgres'
-				, host: 	process.ENV.DB_HOST
-				, port: 	process.ENV.DB_PORT
-				, user: 	'postgres'
-				, password: process.ENV.DB_PASS
-				, database: 'wpm'
-				, models: 	path.join(__dirname, '../schema')
-			});
+		it('should not throw when instantiated', function(){
+			cluster = new Cluster({type:'mysql'});
+		});
 
-			schema.on('load', done);
-		});	
-
-		it('should be able to insert records', function(done){
-			new schema.user({}).save().exec(function(err, usr){
-				if (!err) {
-					assert.ok(usr);
-					user = usr;
-				}
-				done(err);				
+		it('should accept new nodes', function(){
+			cluster.addNode('readwrite', {
+				  username: 	'eb-client'
+				, password: 	''
+				, host: 		'10.0.100.1'
 			});
 		});
 
-		it('should respond with with matching records', function(done){
-			var qb = new schema.user().query();
-			qb.where({id: user.id}).select('id').exec(function(err, usr){
-				if (!err) {
-					assert.ok(usr);
-					assert.ok(usr.length === 1);
-					assert.deepEqual(JSON.stringify(user), JSON.stringify(usr[0]));
-				}
-				done(err);	
-			});
-		});	
-
-		it('should be able to delete records', function(done){
-			new schema.user({id: user.id}).fetch().exec(function(err, usr){
-				if (!err) {
-					assert.ok(usr);
-					usr.destroy().exec(done);					
-				}
-				else done(err);	
-			});
-		});		
+		it('should be able to return an open conenction', function(done) {
+			cluster.getConnection(done);
+		});
 	});
 	
