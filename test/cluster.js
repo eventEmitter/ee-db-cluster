4fded22e7129b9824e1470ad5a59dca2e2398d1e
em <michael@joinbox.com>
2015-11-05 16:09:18 +0100

    works basically

--- test/cluster.js
+++ test/cluster.js
@@ -4,22 +4,35 @@
 
     var   Class                 = require('ee-class')
         , log                   = require('ee-log')
-        , assert                = require('assert');
+        , assert                = require('assert')
+        ;
 
 
 
     var   Cluster               = require('../')
         , PostgresConnection    = require('related-postgres-connection')
-        , config = {};
+        , config
+        ;
 
+
+
+    config = {
+          db: 'test'
+        , maxConnections: 10
+    };
 
 
 
 
     // try to load the local test config
     try {
-        config = require('../test-config.js')
+        let localConfig = require('../test-config.js');
+
+        for (let key in localConfig) config[key] = localConfig[key];
     } catch(e) {}
+
+    
+
 
 
 
@@ -29,12 +42,55 @@
         });
 
 
+
         it('should be able to load a node', function(done) {
             this.timeout(10000);
 
             let cluster = new Cluster(null, PostgresConnection);
 
             cluster.addNode(config).then(done).catch(done);
+        });
+
+
+
+        it('should be able to execute a query', function(done) {
+            this.timeout(10000);
+
+            let cluster = new Cluster(null, PostgresConnection);
+
+            cluster.addNode(config).then(() => {
+                return cluster.query({
+                      SQL: 'INSERT INTO related_db_cluster.test (id) VALUES (default) RETURNING id;'
+                    , pool: 'write'
+                    , mode: 'insert'
+                }).then((record) => {
+                    assert(record && record.type && record.values && record.values.id);
+                    done();
+                });
+            }).catch(done);
+        });
+
+
+
+        it('should be able to insert 1000 items', function(done) {
+            this.timeout(10000);
+
+            let cluster = new Cluster(null, PostgresConnection);
+
+            cluster.addNode(config).then(() => {
+                return Promise.all(Array.apply(null, {length: 1000}).map(() => {
+                    return cluster.query({
+                          SQL: 'INSERT INTO related_db_cluster.test (id) VALUES (default) RETURNING id;'
+                        , pool: 'write'
+                        , mode: 'insert'
+                    }).then((record) => {
+                        assert(record && record.type && record.values && record.values.id);
+                        return Promise.resolve();
+                    });
+                })).then(() => {
+                    done();
+                });               
+            }).catch(done);
         });
     });    
 })();
