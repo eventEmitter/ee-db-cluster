2b3d839f27fee8398355f922c02ae3014b6635fe
em <michael@joinbox.com>
2015-12-20 22:22:24 +0100

    test debugging

--- test/sql/tables.sql
+++ test/sql/tables.sql
@@ -3,8 +3,4 @@
 DROP SCHEMA IF EXISTS related_db_cluster CASCADE;
 CREATE SCHEMA related_db_cluster;
 
-CREATE TABLE related_db_cluster."testTable" (
-      id                serial NOT NULL
-    , title             varchar(10)
-    , CONSTRAINT "pk_testTable" PRIMARY KEY (id)
-);
+CREATE TABLE related_db_cluster."testTable" (id serial NOT NULL, CONSTRAINT "pk_testTable" PRIMARY KEY (id));
