08da69a802627e533f7446e82c5cbfb6000ef94e
em <michael@joinbox.com>
2015-12-20 22:14:08 +0100

    test debugging

--- test/sql/tables.sql
+++ test/sql/tables.sql
@@ -5,5 +5,6 @@ CREATE SCHEMA related_db_cluster;
 
 CREATE TABLE related_db_cluster.test (
       id                serial NOT NULL
+    , title             varchar(10)
     , CONSTRAINT "pk_test" PRIMARY KEY (id)
 );
